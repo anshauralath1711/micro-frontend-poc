e790f791b143be782ec21954c7f9884224d910c0
saurav <souravmajumdar.developer@gmail.com>
2024-04-17 01:23:41 +0530

    Setting up repo

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Marketing from './components/Marketing';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Container</h1>
+      <hr />
+      <Marketing />
+    </div>
+  );
+};
+
+export default App;
