5580a4b64ff4922a51133bb32d5603c61d94a0fa
saurav <souravmajumdar.developer@gmail.com>
2024-04-17 21:19:50 +0530

    Complete auth module

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -5,6 +5,9 @@ const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:3000/',
+  },
   devServer: {
     port: 3000,
     historyApiFallback: true,
@@ -14,6 +17,7 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:3001/remoteEntry.js',
+        auth: 'auth@http://localhost:3002/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Marketing from './components/Marketing';
-import Header from './components/Header';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core';
+import Header from './components/Header';
+import Progress from './components/Progress';
+const MarketingLazy = lazy(() => import('./components/Marketing'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'container',
 });
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <div>
-          <Header />
-          <Marketing />
+          <Header signedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path='/auth'>
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route exact path='/' component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
